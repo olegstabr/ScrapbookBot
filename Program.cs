194a6cf6465fd05ccb2504d8f3ce9775bdce739c
olegstabr <olegstabr@gmail.com>
2018-03-13 20:50:36 +0700

    Add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScrapbookBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
